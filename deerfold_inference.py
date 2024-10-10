29011c7dbdaff8a3aa5171ef7425cd4a8ca2b994
wtq18 <wutianqi1@gmail.com>
2024-10-10 15:17:59 -0500

    Add deerfold inference scriptss

---
+++ deerfold_inference.py
@@ -0,0 +1,218 @@
+import sys
+import os
+import csv
+import argparse
+import numpy as np
+import pandas as pd
+import subprocess
+from Bio import SeqIO
+import MDAnalysis
+import chilife as xl
+from scipy.stats import wasserstein_distance
+from deerfold_processor import run_deerfold
+
+def cdf(arr):
+    return np.cumsum(arr) / np.sum(arr)
+
+def calculate_emd(pdb_file_1, input_csv, pairs):
+    emd = []
+    sigma = 2
+    r = np.linspace(1, 100, 100)
+    mdaload1 = MDAnalysis.Universe(pdb_file_1)
+    for i, j in pairs:
+        P = xl.distance_distribution(xl.SpinLabel.from_mmm('R1M', site=i, chain='A', protein=mdaload1), xl.SpinLabel.from_mmm('R1M', site=j, chain='A', protein=mdaload1), r=r)
+        a = np.argmax(P)
+        maxValue = r[a]
+        gauss = 1/np.sqrt(2*np.pi*sigma**2)*np.exp(-(r-maxValue)**2/(2*sigma**2))
+        dist1 = gauss/np.sum(gauss)
+        peak_index = np.argmax(dist1)
+        left_index = max(0, peak_index - 8)
+        right_index = min(len(dist1) - 1, peak_index + 8)
+        dist1 = dist1[left_index:right_index + 1]
+
+        dist2 = input_csv[(i, j)]
+        emd.append(wasserstein_distance(cdf(np.asarray(P)), cdf(dist2)))
+    return np.mean(emd)
+
+def get_rmsd(pdb1, pdb2, tmscore_path="./TMscore"):
+    result = subprocess.run([tmscore_path, pdb1, pdb2], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+
+    for line in output.split("\n"):
+        if "RMSD of  the common residues" in line and "=" in line:
+            return float(line.split("=")[1].split()[0])
+    return None
+
+def get_tmscore(pdb1, pdb2, tmscore_path="./TMscore"):
+    result = subprocess.run([tmscore_path, pdb1, pdb2], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+
+    for line in output.split("\n"):
+        if "TM-score" in line and "=" in line:
+            return float(line.split("=")[1].split()[0])
+    return None
+
+def create_empty_csv(file_path):
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    
+    # Create an empty CSV file
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+    print(f"Empty CSV file created at: {file_path}")
+
+
+def main(args):
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+
+    model_files = args.models.split(',')
+    pdbid, _ = os.path.splitext(os.path.basename(args.fasta))
+    num_per_model = args.num_models // len(model_files)  # Divide equally among 3 models
+    remainder = args.num_models % len(model_files)  # Handle any remainder
+
+    pairs = []
+    input_csv = {}
+    if args.splabel is None or os.path.getsize(args.splabel) == 0:
+        print(f"# of distance constraints: 0")
+        args.splabel = f"{args.outdir}/empty.csv"
+        create_empty_csv(args.splabel)
+    else:
+        df = pd.read_csv(args.splabel, header=None)
+        count = 0
+        residues = set()
+        for _, row in df.iterrows():
+            i, j = int(row[0]), int(row[1])
+            input_csv[(i, j)] = np.array(row[2:])
+            pairs.append((i,j))
+            count += 1
+            residues.add(i)
+            residues.add(j)
+        print(f"{args.splabel} # of distance constraints: {count}, residues are {residues}")
+
+    model_num = 1
+    for model_idx, model_file in enumerate(model_files):
+        num_for_this_model = num_per_model + (1 if model_idx < remainder else 0)
+        for i in range(num_for_this_model):
+            output_path = f"{args.outdir}/{pdbid}_model_{model_num}.pdb"
+            if not os.path.exists(output_path):
+                output_dir, output_name = run_deerfold(
+                    fasta_path=args.fasta,
+                    splabel=args.splabel,
+                    use_precomputed_alignments=args.msa_dir,
+                    output_dir=f"{args.outdir}/{pdbid}",
+                    checkpoint_path=model_file,
+                    neff=args.neff,
+                    distograms=True,
+                    model_device=args.model_device,
+                    features=args.features,
+                    data_random_seed=None,
+                    skip_relaxation=True,
+                    save_outputs=True
+                )
+                os.rename(f"{output_dir}/{output_name}_unrelaxed.pdb", output_path)
+                os.system(f"rm -r {output_dir}")
+                print(f"{output_path} .......Complete")
+            else:
+                print(f"{output_path} existed....Skip")
+            model_num += 1
+
+    if pairs:
+        results2 = {}
+        for i in range(args.num_models):
+            emd_dist = calculate_emd(f"{args.outdir}/{pdbid}_model_{i + 1}.pdb", input_csv, pairs)
+            results2[f"{pdbid}_model_{i + 1}.pdb"] = emd_dist
+            print(f"Calculating emd scores for {pdbid}_model_{i + 1}.pdb....Done")
+
+        sorted_result = sorted(results2.items(), key=lambda x: x[1])
+        top_n = sorted_result[:args.num_models]
+        # print(top_n)
+
+        rename_operations = []
+        for i, (old_name, _) in enumerate(top_n, start=1):
+            temp_name = f"{args.outdir}/temp_{i}.pdb"
+            final_name = f"{args.outdir}/{pdbid}_model_{i}.pdb"
+            rename_operations.append((f"{args.outdir}/{old_name}", temp_name, final_name))
+
+        for old_name, temp_name, final_name in rename_operations:
+            os.rename(old_name, temp_name)
+
+        for _, temp_name, final_name in rename_operations:
+            os.rename(temp_name, final_name)
+    
+    print("\n")
+    for ref_pdb in args.ref_pdbs.split(','):
+        results2 = {}
+        tms = {}
+        for i in range(args.num_models):
+            if not os.path.exists(f"{args.outdir}/{pdbid}_model_{i + 1}.pdb"):
+                continue
+            rmsd = get_rmsd(f"{args.outdir}/{pdbid}_model_{i + 1}.pdb", f"{ref_pdb}")
+            results2[f"{pdbid}_model_{i + 1}.pdb"] = rmsd
+            tmscore = get_tmscore(f"{args.outdir}/{pdbid}_model_{i + 1}.pdb", f"{ref_pdb}")
+            tms[f"{pdbid}_model_{i + 1}.pdb"] = tmscore
+
+        mean_value = sum(results2.values()) / len(results2)
+        min_value = min(results2.values())
+        min_key = [key for key, value in results2.items() if value == min_value]
+        print(f"Average RMSD between prediction and {ref_pdb}: {mean_value}")
+        print(f"Best RMSD between prediction({min_key}) and {ref_pdb}: {min_value}")
+        print(results2, "\n")
+        
+        mean_value = sum(tms.values()) / len(tms)
+        max_value = max(tms.values())
+        max_key = [key for key, value in tms.items() if value == max_value]
+        print(f"Average TMscore between prediction and {ref_pdb}: {mean_value}")
+        print(f"Best TMscore between prediction({max_key}) and {ref_pdb}: {max_value}")
+        print(tms, "\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="DEERFold processing script")
+    parser.add_argument(
+        "fasta", type=str,
+        help="Path to FASTA file, one sequence per file"
+    )
+    parser.add_argument(
+        "--splabel", type=str,
+        help="Path to the spin label CSV file"
+    )
+    parser.add_argument(
+        "--msa_dir", type=str, required=True,
+        help="Directory containing precomputed MSA"
+    )
+    parser.add_argument(
+        "--neff", type=float, default=5.0,
+        help="Neff value for MSA subsampling"
+    )
+    parser.add_argument(
+        "--models", type=str, default="models/DEERFold_helix.pt,models/DEERFold_strand.pt,models/DEERFold_com.pt",
+        help="Model weights (default: path/to/default/model.pth)"
+    )
+    parser.add_argument(
+        "--save_outputs", action="store_true", default=True,
+        help="Whether to save all model outputs, including embeddings, etc."
+    )
+    parser.add_argument(
+        "--features", type=str,
+        help="Feature pickle"
+    )
+    parser.add_argument(
+        "--model_device", type=str, default="cuda:0",
+        help="""Name of the device on which to run the model. Any valid torch
+             device name is accepted (e.g. "cpu", "cuda:0")"""
+    )
+    parser.add_argument(
+        "--ref_pdbs", type=str,
+        help="Comma-separated list of reference PDB files"
+    )
+    parser.add_argument(
+        "--outdir", type=str, required=True,
+        help="Output directory for results"
+    )
+    parser.add_argument(
+        "--num_models", type=int, default=15,
+        help="Total number of models to generate (default: 15)"
+    )
+
+    args = parser.parse_args()
+    main(args)
---
+++ deerfold_processor.py
@@ -0,0 +1,340 @@
+import argparse
+from copy import deepcopy
+from datetime import date
+import logging
+import math
+import numpy as np
+import os
+
+logging.basicConfig()
+logger = logging.getLogger(__file__)
+logger.setLevel(level=logging.INFO)
+
+import pickle
+import random
+import sys
+import time
+import torch
+import re
+from Bio import SeqIO
+import pandas as pd
+
+torch_versions = torch.__version__.split(".")
+torch_major_version = int(torch_versions[0])
+torch_minor_version = int(torch_versions[1])
+if(
+    torch_major_version > 1 or 
+    (torch_major_version == 1 and torch_minor_version >= 12)
+):
+    # Gives a large speedup on Ampere-class GPUs
+    torch.set_float32_matmul_precision("high")
+
+torch.set_grad_enabled(False)
+
+from openfold.config import model_config
+from openfold.data import feature_pipeline, data_pipeline
+from openfold.model.model import AlphaFold
+from openfold.np import residue_constants, protein
+import openfold.np.relax.relax as relax
+from openfold.utils.tensor_utils import (
+    tensor_tree_map,
+)
+from openfold.data.msa_subsampling import subsample_msa_sequentially
+
+# Global configuration class
+class Config:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+# Global configuration instance
+config = Config()
+
+def precompute_alignments(tag, seq, alignment_dir):
+    tmp_fasta_path = os.path.join(config.output_dir, f"tmp_{os.getpid()}.fasta")
+    with open(tmp_fasta_path, "w") as fp:
+        fp.write(f">{tag}\n{seq}")
+
+    local_alignment_dir = os.path.join(alignment_dir, tag)
+
+    if(config.use_precomputed_alignments is None and not os.path.isdir(local_alignment_dir)):
+        logger.info(f"Generating alignments for {tag}...")
+            
+        os.makedirs(local_alignment_dir)
+
+        alignment_runner = data_pipeline.AlignmentRunner(
+            jackhmmer_binary_path=config.jackhmmer_binary_path,
+            hhblits_binary_path=config.hhblits_binary_path,
+            hhsearch_binary_path=config.hhsearch_binary_path,
+            uniref90_database_path=config.uniref90_database_path,
+            mgnify_database_path=config.mgnify_database_path,
+            bfd_database_path=config.bfd_database_path,
+            uniclust30_database_path=config.uniclust30_database_path,
+            pdb70_database_path=config.pdb70_database_path,
+            no_cpus=config.cpus,
+        )
+        alignment_runner.run(
+            tmp_fasta_path, local_alignment_dir
+        )
+    else:
+        logger.info(
+            f"Using precomputed alignments for {tag} at {alignment_dir}..."
+        )
+
+    # Remove temporary FASTA file
+    os.remove(tmp_fasta_path)
+
+def run_model(model, batch, tag):
+    with torch.no_grad(): 
+        # Disable templates if there aren't any in the batch
+        model.config.template.enabled = model.config.template.enabled and any([
+            "template_" in k for k in batch
+        ])
+
+        logger.info(f"Running inference for {tag}...")
+        t = time.perf_counter()
+        out = model(batch)
+        inference_time = time.perf_counter() - t
+        logger.info(f"Inference time: {inference_time}")
+   
+    return out
+
+def parse_fasta(data):
+    data = re.sub('>$', '', data, flags=re.M)
+    lines = [
+        l.replace('\n', '')
+        for prot in data.split('>') for l in prot.strip().split('\n', 1)
+    ][1:]
+    tags, seqs = lines[::2], lines[1::2]
+
+    tags = [t.split()[0] for t in tags]
+
+    return tags, seqs
+
+def generate_feature_dict(
+    tags,
+    seqs,
+    alignment_dir,
+    data_processor
+):
+    tmp_fasta_path = os.path.join(config.output_dir, f"tmp_{os.getpid()}.fasta")
+    if len(seqs) == 1:
+        tag = tags[0]
+        seq = seqs[0]
+        with open(tmp_fasta_path, "w") as fp:
+            fp.write(f">{tag}\n{seq}")
+
+        local_alignment_dir = os.path.join(alignment_dir, tag)
+        feature_dict = data_processor.process_fasta(
+            fasta_path=tmp_fasta_path, alignment_dir=local_alignment_dir
+        )
+    else:
+        with open(tmp_fasta_path, "w") as fp:
+            fp.write(
+                '\n'.join([f">{tag}\n{seq}" for tag, seq in zip(tags, seqs)])
+            )
+        feature_dict = data_processor.process_multiseq_fasta(
+            fasta_path=tmp_fasta_path, super_alignment_dir=alignment_dir,
+        )
+
+    # Remove temporary FASTA file
+    os.remove(tmp_fasta_path)
+
+    return feature_dict
+
+def get_model_basename(model_path):
+    return os.path.splitext(
+                os.path.basename(
+                    os.path.normpath(model_path)
+                )
+            )[0]
+
+def make_output_directory(output_dir, model_name, multiple_model_mode):
+    if multiple_model_mode:
+        prediction_dir = os.path.join(output_dir, model_name)
+    else:
+        prediction_dir = os.path.join(output_dir)
+    os.makedirs(prediction_dir, exist_ok=True)
+    return prediction_dir
+
+def count_models_to_evaluate(openfold_checkpoint_path, jax_param_path):
+    model_count = 0
+    if openfold_checkpoint_path:
+        model_count += len(openfold_checkpoint_path.split(","))
+    if jax_param_path:
+        model_count += len(jax_param_path.split(","))
+    return model_count
+
+def load_model():
+    # Create the output directory
+    model = AlphaFold(config.model_config)
+    model = model.eval()
+    checkpoint_basename = get_model_basename(config.checkpoint_path)
+
+    sd = torch.load(config.checkpoint_path)['ema']['params']
+    new_sd = {key.replace('module.model.', ''): value for key, value in sd.items()}
+
+    model.load_state_dict(new_sd)
+    
+    model = model.to(config.model_device)
+    logger.info(
+        f"Loaded OpenFold parameters at {config.checkpoint_path}..."
+    )
+    return model
+
+def list_files_with_extensions(dir, extensions):
+    return [f for f in os.listdir(dir) if f.endswith(extensions)]
+
+
+def fill_org_dist(fasta_file, sp_csv):
+    record = SeqIO.read(fasta_file, 'fasta')
+    n = len(record.seq)
+    org_dist = np.zeros((n, n, 100))
+
+    if os.path.getsize(sp_csv) != 0:
+        df = pd.read_csv(sp_csv, header=None)
+
+        for _, row in df.iterrows():
+            i, j = int(row[0]), int(row[1])
+            org_dist[i - 1][j - 1] = org_dist[j - 1][i - 1] = row[2:].to_numpy()
+    
+    return org_dist
+
+def run_deerfold(**kwargs):
+    """
+    Main function to run the DEERFold pipeline.
+    Updates the global config with provided kwargs and executes the pipeline.
+    """
+    # Update global configuration
+    config.__dict__.update(kwargs)
+    
+    os.makedirs(config.output_dir, exist_ok=True)
+
+    config.model_config = model_config('model_5_ptm')
+    data_processor = data_pipeline.DataPipeline(
+        template_featurizer=None,
+    )
+
+    random_seed = config.data_random_seed
+    if random_seed is None:
+        random_seed = random.randrange(2**32)
+    
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed + 1)
+    
+    feature_processor = feature_pipeline.FeaturePipeline(config.model_config.data)
+
+    if config.use_precomputed_alignments is None:
+        alignment_dir = os.path.join(config.output_dir, "alignments")
+    else:
+        alignment_dir = config.use_precomputed_alignments
+
+
+    with open(config.fasta_path, "r") as fp:
+        data = fp.read()
+
+    tag, seq = parse_fasta(data)
+    tag = tag[0]
+    seq = seq[0]
+
+    model = load_model()
+    output_directory = make_output_directory(config.output_dir, get_model_basename(config.checkpoint_path), False)
+
+    output_name = f'{tag}_model_1'
+
+    if config.features:
+        feature_dict = pickle.load(open(config.features,'rb'))
+    else:
+        # Does nothing if the alignments have already been computed
+        precompute_alignments(tag, seq, alignment_dir)
+        feature_dict = generate_feature_dict(
+            [tag],
+            [seq],
+            alignment_dir,
+            data_processor,
+        )
+
+    if config.splabel.endswith('.csv'):
+        sp = fill_org_dist(config.fasta_path, config.splabel)
+        feature_dict['sp'] = sp
+    else:
+        logger.error("Input distance info need to be in CSV file")
+        return
+
+    # subsample MSAs to specified Neff
+    msa = feature_dict['msa']
+
+    if config.neff:
+        logger.info(
+            f"Subsampling MSA to Neff={config.neff}..."
+        )
+        indices = subsample_msa_sequentially(msa, neff=config.neff, eff_cutoff=0.62, cap_msa=False)
+        feature_dict['msa'] = msa[indices]
+        feature_dict['deletion_matrix_int'] = feature_dict['deletion_matrix_int'][indices]
+#     print(feature_dict['msa'].shape, feature_dict['deletion_matrix_int'].shape)
+
+    processed_feature_dict = feature_processor.process_features(
+        feature_dict, mode='predict',
+    )
+
+    processed_feature_dict = {
+        k:torch.as_tensor(v, device=config.model_device) 
+        for k,v in processed_feature_dict.items()
+    }
+
+    out = run_model(model, processed_feature_dict, tag)
+
+    # Toss out the recycling dimensions --- we don't need them anymore
+    processed_feature_dict = tensor_tree_map(
+        lambda x: np.array(x[..., -1].cpu()), 
+        processed_feature_dict
+    )
+    out = tensor_tree_map(lambda x: np.array(x.cpu()), out)
+
+    plddt = out["plddt"]
+
+    plddt_b_factors = np.repeat(
+        plddt[..., None], residue_constants.atom_type_num, axis=-1
+    )
+
+    unrelaxed_protein = protein.from_prediction(
+        features=processed_feature_dict,
+        result=out,
+        b_factors=plddt_b_factors
+    )
+
+    unrelaxed_output_path = os.path.join(
+        output_directory, f'{output_name}_unrelaxed.pdb'
+    )
+
+
+    with open(unrelaxed_output_path, 'w') as fp:
+        fp.write(protein.to_pdb(unrelaxed_protein))
+
+    logger.info(f"Output written to {unrelaxed_output_path}...")
+
+    if not config.skip_relaxation:        
+        amber_relaxer = relax.AmberRelaxation(
+            **config.relax
+        )
+        
+        relaxed_pdb_str, _, _ = amber_relaxer.process(prot=unrelaxed_protein)
+
+        # Save the relaxed PDB.
+        relaxed_output_path = os.path.join(
+            output_directory, f'{output_name}_relaxed.pdb'
+        )
+        with open(relaxed_output_path, 'w') as fp:
+            fp.write(relaxed_pdb_str)
+        
+        logger.info(f"Relaxed output written to {relaxed_output_path}...")
+
+    if config.save_outputs:
+        output_dict_path = os.path.join(
+            output_directory, f'{output_name}_output_dict.pkl'
+        )
+        with open(output_dict_path, "wb") as fp:
+            pickle.dump(feature_dict['msa'], fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+        logger.info(f"Model output written to {output_dict_path}...")
+    
+    return output_directory, output_name
